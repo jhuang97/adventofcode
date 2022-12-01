228be68561abdf422f3537e61ed4942e4c87225e
Jeffrey Huang <jeffreyhuang97@hotmail.com>
2022-11-30 23:33:25 -0600

    Delete aoc22 directory

--- aoc22/day01.py
+++
@@ -1,8 +0,0 @@
-file1 = open('day1_input.txt', 'r')
-lines = file1.read().split('\n\n')
-
-cals = [sum([int(s) for s in a.splitlines()]) for a in lines]
-print(max(cals))
-
-cals.sort()
-print(sum(cals[-3:]))
